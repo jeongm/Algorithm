616604e0fa984c3e58e708d0c0f51be20a0ba5c0
Jeong <57540375+jeongm@users.noreply.github.com>
2023-07-03 19:35:41 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,20 +1,20 @@
 T = int(input())
 
-for i in range(T):
-    vps = input()
-    end = 0;left = 0;right=0
-    for ps in vps:
-        end+=1
-        if ps == '(':
-            left += 1
+result = ''
+
+for _ in range(T):
+    ps = input()
+    left =0; right =0
+    for p in ps:
+        if p == '(' :
+            left +=1
         else :
-            right += 1
+            right +=1
+
         if right > left :
-            print('NO')
-            break;
-        elif end == len(vps):
-            if left == right:
-                print('YES')
-            else :
-                print('NO')
-                
+            break
+    if left == right:
+        result = 'YES'
+    else :
+        result = 'NO' 
+    print(result)
