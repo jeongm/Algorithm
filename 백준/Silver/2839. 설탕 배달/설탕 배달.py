6b08fa4859b7d819a008653c0f53576949625c1c
Jeong <57540375+jeongm@users.noreply.github.com>
2023-07-06 17:51:16 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,12 +1,13 @@
 N = int(input())
-suger = -1
 
-for i in range(N//5,-1,-1):
-    n = N
-    n -= 5*i
-    n -= n//3*3
-    if n == 0:
-        suger = i + (N-5*i)//3
-        break
-    
-print(suger)
+result = N
+for i in range(N//5+1):
+    tmp = N-5*i
+
+    if tmp%3 == 0 :
+        result = min(i + tmp/3,result)
+
+if result == N:
+    print(-1)
+else :
+    print(int(result))
