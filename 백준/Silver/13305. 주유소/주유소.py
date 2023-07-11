ec6c8f1e5eee90f06a005df892c753b20e2120cf
Jeong <57540375+jeongm@users.noreply.github.com>
2023-07-11 14:09:38 +0900

    [Silver III] Title: 주유소, Time: 116 ms, Memory: 46584 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,23 +1,12 @@
 N = int(input())
+roads = list(map(int,input().split()))
+oils = list(map(int,input().split()))
 
-road = list(map(int, input().split()))
-city_oil = list(map(int, input().split()))
+oil = oils[0]
+total = 0
 
-road_size=0 # 전체 도로 길이
-mini_oil= city_oil[0] # 가장 저렴한 도시 오일 가격
-
-for i in range(N-1):
-    if mini_oil > city_oil[i]:
-        mini_oil=city_oil[i]
-    road_size+=road[i]
-    
-    mini_cost=0
-for i in range(N):
-    if mini_oil == city_oil[i]:
-        mini_cost += city_oil[i]*road_size
-        break
-    else :
-        mini_cost += city_oil[i]*road[i]
-        road_size -= road[i]
-
-print(mini_cost)
+for i in range(1,N):
+    total += roads[i-1] * oil
+    if oil > oils[i]:
+        oil = oils[i]
+print(total)
