43a8a90648c8a2e748de011fde532c92ac44e7a2
Jeong <jeongm0220@gmail.com>
2023-09-13 20:17:36 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,20 +1,13 @@
 N = int(input())
 
-count = 0
+count = N
 for _ in range(N):
-    alpa  = [False] *26
-    flag = 0
     word = input()
-    chr = ''
-    for i in range(0,len(word)):
-        if chr != word[i]:
-            if alpa[ord(word[i])-97] == True:
-                break
-            else :
-                alpa[ord(word[i])-97] = True
-        chr = word[i]
-        flag+=1
-    if flag == len(word) :
-        count +=1
+    for i in range(len(word)-1):
+        if word[i] == word[i+1]:
+            pass
+        elif word[i] in word[i+1:]:
+            count-=1
+            break
 
 print(count)
