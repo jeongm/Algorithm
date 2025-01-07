a7aa3790d6d98454f94936132f4175b31b4eec38
Jeong <jeongm0220@gmail.com>
2025-01-07 11:24:14 +0900

    [Silver III] Title: 후위 표기식2, Time: 112 ms, Memory: 14456 KB -BaekjoonHub

---
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+    
+        public static void main(String[] args) throws Exception {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int N = Integer.parseInt(br.readLine());
+            String pstfExpr = br.readLine();
+            double[] alpa = new double[26];
+
+            for(int i = 0; i < pstfExpr.length(); i++) {
+                if(pstfExpr.charAt(i)-0 >= 65 && pstfExpr.charAt(i)-0 <= 90 && alpa[pstfExpr.charAt(i)-65] == 0) {
+                    alpa[pstfExpr.charAt(i)-65] = Double.parseDouble(br.readLine());
+                } 
+            }
+            
+            Stack<Double> stack = new Stack<>();
+            for(int i = 0; i < pstfExpr.length(); i++) {
+                if(pstfExpr.charAt(i)-0 >= 65 && pstfExpr.charAt(i)-0 <= 90) {
+                    stack.push(alpa[pstfExpr.charAt(i)-65]);
+                }else if(pstfExpr.charAt(i) == '+'){
+                    double b = stack.pop();
+                    double a = stack.pop();
+                    stack.push(a+b);
+                }else if(pstfExpr.charAt(i) == '-'){
+                    double b = stack.pop();
+                    double a = stack.pop();
+                    stack.push(a-b);
+                }else if(pstfExpr.charAt(i) == '*'){
+                    double b = stack.pop();
+                    double a = stack.pop();
+                    stack.push(a*b);
+                }else if(pstfExpr.charAt(i) == '/'){
+                    double b = stack.pop();
+                    double a = stack.pop();
+                    stack.push(a/b);
+                }                
+            }
+
+            System.out.println(String.format("%.2f",stack.pop()));            
+
+    }
+
+}
