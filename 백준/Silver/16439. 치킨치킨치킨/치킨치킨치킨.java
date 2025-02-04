461f9294200659b13256bdb6e5a0e9428f638265
Jeong <jeongm0220@gmail.com>
2025-02-04 12:12:41 +0900

    [Silver IV] Title: 치킨치킨치킨, Time: 116 ms, Memory: 14808 KB -BaekjoonHub

--- 백준/Silver/16439. 치킨치킨치킨/치킨치킨치킨.java
+++ 백준/Silver/16439. 치킨치킨치킨/치킨치킨치킨.java
@@ -1,50 +1,58 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M = 0;
+    static int N;
+    static int M;
     static int[][] likeChicken;
-    static int bestPreference = 0;
-    public static void main(String[] args) throws Exception {
+    static int bestSum = 0;
 
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
 
+        N = Integer.parseInt(st.nextToken()); // 고리 회원의 수
+        M = Integer.parseInt(st.nextToken()); // 치킨 종류의 수
         likeChicken = new int[N][M];
+
+        //치킨 선호도
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < M; j++) {
                 likeChicken[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-    
-        dfs(0, 0,new int[3]);
 
-        System.out.println(bestPreference);
+        bfs(0, 0, new int[3]);
+
+        System.out.println(bestSum);
 
 
     }
 
-    static void dfs(int start, int depth, int[] selectChicken) {
-        if(depth == 3) {
-            int preference = 0;
+    static void bfs(int start, int selectCnt, int[] selectChicken) {
+        // 선택한 치킨 인덱스
+        if(selectCnt == 3) {
+            int bestTmp = 0;
             for(int i = 0; i < N; i++) {
-                int bestLike = Math.max(likeChicken[i][selectChicken[0]], Math.max(likeChicken[i][selectChicken[1]], likeChicken[i][selectChicken[2]]));
-                preference += bestLike;
+                int best = likeChicken[i][selectChicken[0]];
+                if(best < likeChicken[i][selectChicken[1]]) best = likeChicken[i][selectChicken[1]];
+                if(best < likeChicken[i][selectChicken[2]]) best = likeChicken[i][selectChicken[2]];
+                bestTmp+=best;
             }
-            if(preference > bestPreference)
-                bestPreference = preference;
+
+            if(bestSum < bestTmp) bestSum = bestTmp;
+
             return;
         }
 
         for(int i = start; i < M; i++) {
-            selectChicken[depth] = i;
-            dfs(i+1, depth+1, selectChicken);
+            selectChicken[selectCnt] = i;
+            bfs(i+1,selectCnt+1,selectChicken);
         }
     }
-
-    
 }
